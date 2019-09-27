e47bde6eac0619baad25424fa0a4985d403df512
jiaxin <edison7500@gmail.com>
2019-09-27 16:50:06 +0800

    add test case

---
+++ demo/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class PyAvagenViewTestCase(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_avatar_generate_view(self):
+        url = reverse("pyavagen:generator", args=[300, 'Trump'])
+
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
